dce8453fb90b3d7d15f76784bf46f26e6d3154c2
Nishchal Sharma <deathknightultra@gmail.com>
2019-10-16 08:02:44 +0530

    First DreamCollector Committ

---
+++ main.py
@@ -0,0 +1,221 @@
+import random
+import math
+
+cap_mul=2
+vcap_mul=1.5
+
+class player:
+    def __init__(self,name):
+        self.name=name
+        self.score=0
+        self.iscap=False
+        self.isvcap=False
+    def makeCap(self):
+        self.iscap=True
+    def makeVCap(self):
+        self.isvcap=True
+
+    def getscore(self):
+        if(self.iscap):
+            return(cap_mul*self.score)
+        elif(self.isvcap):
+            return(vcap_mul*self.score)
+        else:
+            return(self.score)
+
+    def display(self):
+        s="Name:"+str(self.name)
+        if(self.iscap):
+            s=s+"(C)"
+        elif(self.isvcap):
+            s=s+"(VC)"
+        else:
+            pass
+        s=s+" "+str(self.getscore())
+        print(s)
+#--------------------------------------------------------
+class team:
+    def __init__(self):
+        self.name=""
+        self.players=[]
+        self.count=11
+        self.filled=0
+        self.GrandScore=0
+    def updateGrandScore(self):
+        self.GrandScore+=self.getTotal()
+
+    def addplayer(self,player):
+        if(self.filled<self.count):
+            self.players.append(player)
+            self.filled+=1
+            
+    def totalScore(self):
+        s=0
+        for p in self.players:
+            s=s+p.score()
+        return(s)
+
+    def display(self):
+        print("***-----***")
+        for p in self.players:
+            p.display()
+        print("***-----***")
+
+    def displayTotal(self):
+        t=0
+        for p in self.players:
+            t+=p.getscore()
+        print("T>>>"+str(t))
+    def getTotal(self):
+        t=0
+        for p in self.players:
+            t+=p.getscore()
+        return(t)
+
+#-----------------------------------------------------------------
+class selectionlist:
+    def __init__(self,f):
+        self.teams=[]
+        self.chancelist={}
+        self.scorelist={}
+        self.playername=[]
+        self.file=f
+    
+
+    def createList(self):
+        f=open(self.file,"r")
+        lines =f.readlines()
+        for line in lines:
+            player,chance=line.split(",")
+            player=player.rstrip("\n")
+            chance=chance.rstrip("\n")
+            self.playername.append(player)
+            self.chancelist[player]=chance
+            self.scorelist[player]=0
+
+    def generateTeam(self):
+        t=team()
+        c=0
+        a=0
+        l=len(self.playername)
+        for p in self.playername:
+            r=random.random()*100
+            if((11-a)<(l-c)):
+                if(r<int(self.chancelist[p])):
+                    #print(">>>>"+str(r)+"<<<<"+str(self.chancelist[p]))
+                    plr=player(p)
+                    t.addplayer(plr)
+                    a=a+1
+            else:
+                plr=player(p)
+                t.addplayer(plr)
+                a=a+1
+            c=c+1
+                    
+
+        x=random.randrange(0,11)
+        y=random.randrange(0,11)
+
+        t.players[x].makeCap()
+        t.players[y].makeVCap()
+        return(t)
+    def createRandomTeams(self,n):
+        for i in range(n):
+            self.teams.append(self.generateTeam())
+    
+    def assignScore(self):
+        for p in self.playername:
+            sc=math.floor(random.randrange(-1,90))
+            self.scorelist[p]=sc
+    def updateAllTeams(self):
+        for t in self.teams:
+            for p in t.players:               
+                p.score=self.scorelist[p.name]
+        for myp in self.myTeam.players:               
+                myp.score=self.scorelist[myp.name]
+    def updateGrandScore(self):
+        for t in self.teams:
+            t.updateGrandScore()
+        self.myTeam.updateGrandScore()
+
+    def display(self):
+        
+        for t in self.teams:
+            t.displayTotal()
+        self.myTeam.displayTotal()
+
+    def createMyTeam(self,l,c,vc):
+        t=team()
+        x=0
+        for l1 in l:
+           # print(">>Adding To my Team",)
+            p=self.playername[l1]
+            #print(p)
+            plr=player(p)
+            if(x==c):
+                plr.makeCap()
+            
+            if(x==vc):
+                plr.makeVCap()
+            
+            t.addplayer(plr)
+            x=x+1
+        self.myTeam=t
+        '''
+        print(">>Finished adding team")
+        for x in t.players:
+            x.display() 
+        '''
+        
+    def dump(self):
+        s=""
+        for t in self.teams:
+            s=s+str(t.GrandScore)+","
+            s=s+str(t.getTotal())
+            for p in t.players:
+                s=s+","+self.chancelist[p.name]+","
+                if(p.iscap):
+                    s=s+"*"+p.name+","+str(p.getscore())
+                elif(p.isvcap):
+                    s=s+"#"+p.name+","+str(p.getscore())
+                else:
+                    s=s+p.name+","+str(p.getscore())
+            s=s+"\n"
+        s=s+"$$$"
+        s=s+str(self.myTeam.GrandScore)+","
+        s=s+str(self.myTeam.getTotal())
+        for p in self.myTeam.players:
+                s=s+","+self.chancelist[p.name]+","
+                if(p.iscap):
+                    s=s+"*"+p.name+","+str(p.getscore())
+                elif(p.isvcap):
+                    s=s+"#"+p.name+","+str(p.getscore())
+                else:
+                    s=s+p.name+","+str(p.getscore())
+        s=s+"\n"
+        sa=open("save.csv","w")
+        sa.write(s)
+
+#-----Code--------------------------------------
+
+s=selectionlist("player.txt")
+l=[0,1,2,3,4,5,6,7,8,9,10]
+#l=[1,2,3,4,8,10,15,13,18,19,21]
+s.createList()
+s.createMyTeam(l,0,1)
+s.createRandomTeams(100)
+for i in range(100000):
+    s.assignScore()
+    s.updateAllTeams()
+    s.updateGrandScore()
+'''
+#s.display()
+print("--------Score List-----------")
+print(s.scorelist)
+print("----------Chance List---------")
+print(s.chancelist)
+'''
+s.dump()
+
+
+
